de38e86112dfca1b7f669a1b717cfcc497a6221f
jloescher <jonathan@jloescher.com>
2021-10-23 21:59:57 -0400

    Added Material UI: Stopped at 21:56 on https://www.youtube.com/watch?v=cLKLqpxPSws&t=93s

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import { Typography } from "@mui/material";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Typography variant="h1">Hello Next World</Typography>
+    </div>
+  );
+};
+
+export default Home;
